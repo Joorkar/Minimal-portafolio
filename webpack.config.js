b6613c4f242de20494ddf934071d93cc9b4712bf
Joorkar Romero <joorkarobrajham@gmail.com>
2021-10-09 19:10:01 -0600

    Tranformando js para todos los navegadores

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
