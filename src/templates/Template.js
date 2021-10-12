52e3a2c7d4e564af0897e0b1c7fc2da97c339f2d
Joorkar Romero <joorkarobrajham@gmail.com>
2021-10-11 19:35:26 -0600

    [ADD] Dev server

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -26,13 +26,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://twitter.com/joorkar_rocha">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/Joorkar">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://www.instagram.com/joorkarrocha">
             <img src="${instagram}" />
           </a>
         </div>
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -66,4 +66,11 @@ module.exports = {
     }),
     new Dotenv(),
   ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 8080,
+    open: true,
+  },
 };
